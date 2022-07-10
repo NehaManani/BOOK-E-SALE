83bc1d199dac4e37f163eb9dcdf3938600dd5487
Nmk1213 <107633867+Nmk1213@users.noreply.github.com>
2022-07-10 19:01:59 +0530

    Add files via upload

---
+++ BookStore.Models/Models/CartModel.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Models.ViewModels;
+
+namespace BookStore.Models.Models
+{
+    public class CartModel
+    {
+        public CartModel() { }
+
+        public CartModel(Cart cart)
+        {
+            Id = cart.Id;
+
+            UserId = cart.Userid;
+
+            BookId = cart.Bookid;
+
+            Quantity = cart.Quantity;
+
+
+            //TODO: add all properties
+        }
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
